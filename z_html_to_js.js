4b7494893d72b8fcd337951acc2f426e9e0d754f
jaldipbhalani <jaldipbhalani@gmail.com>
2022-02-06 20:02:38 +0530

    6-2-22

---
+++ z_html_to_js.js
@@ -0,0 +1,65 @@
+// window.addEventListener('load', (event) => {
+// var a = 188
+// console.log(a)
+// var b = document.getElementById("ii")
+// console.log(b)
+
+// b = document.getElementsByClassName("cc")
+// console.log(b)
+
+// b = document.getElementsByTagName("h3")
+// console.log(b)
+
+// b = document.querySelector("#ii")
+// console.log(b)
+
+// b = document.querySelector(".cc")
+// console.log(b)
+
+// b = document.querySelector("h3")
+// console.log(b)
+
+
+
+
+// parenNode
+//  const c = document.querySelector('#ii');
+//  const c = document.querySelector('#divv');
+//  const p = c.parentNode;
+//  console.log(p);
+
+//childNode
+//  const p=c.childNodes;
+//  console.log(p)
+
+//nextElementSibling
+// const c = document.querySelector('#p1');
+// console.log(c.nextElementSibling)
+
+//previousElementSibling
+// const c = document.querySelector('#h1');
+// console.log(c.previousElementSibling)
+
+
+// c.innerHTML='web dev is awesome!';
+// c.style.color="blue";
+// c.style.fontSize='40px'
+
+// c.classList.add("xyzzzz");
+// c.classList.remove('xyzzzz')
+
+
+const d = document.createElement('h5')
+console.log(d)
+document.h1.appendChild(d)
+// b = document.querySelector("#divv")
+// b.innerHTML = 'javascript is awesome!';
+// console.log(b)
+
+
+
+
+
+
+
+// });
